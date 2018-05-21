edaf32538c8a847ed9d94a6f9d787f7e62d41f1a
Erin Walker <erinb0317@hotmail.com>
2018-05-21 20:19:23 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,50 +1,50 @@
 class SongsController < ApplicationController
 
-  def index 
-    @songs = Song.all 
-  end 
+  def index
+    @songs = Song.all
+  end
 
-  def new 
-    @song = Song.new 
-  end 
+  def new
+    @song = Song.new
+  end
 
-  def show 
-    @song = song 
-  end 
+  def show
+    @song = song
+  end
 
-  def create 
+  def create
     @song = Song.new(song_params)
-    if @song.valid? 
-      @song.save 
-      redirect_to @song 
-    else 
-      render :new 
-    end 
-  end 
+    if @song.valid?
+      @song.save
+      redirect_to @song
+    else
+      render :new
+    end
+  end
 
-  def edit 
-    @song = song 
-  end 
+  def edit
+    @song = song
+  end
 
-  def update 
-    @song = song 
+  def update
+    @song = song
     if @song.update(song_params)
-      redirect_to @song 
-    else 
-      render :edit 
-    end 
-  end 
+      redirect_to @song
+    else
+      render :edit
+    end
+  end
 
-  def destroy 
-    song.destroy 
-    redirect_to songs_path 
-  end 
+  def destroy
+    song.destroy
+    redirect_to songs_path
+  end
 
-private 
+private
 
-  def song 
+  def song
     Song.find(params[:id])
-  end 
+  end
 
   def song_params
     params.require(:song).permit(:title, :release_year, :released, :genre, :artist_name)
